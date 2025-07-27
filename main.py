63598338ac14defad01ffc5292c025133ef23af8
Ayush Pathak <ayushpathak@Masterusers-MacBook-Pro.local>
2025-07-28 02:47:06 +0530

    add debugger

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-
+print("DB_HOST =", DB_HOST)
+print("DB_NAME =", DB_NAME)
+print("DB_USER =", DB_USER)
 # === DB Connection ===
 conn = psycopg2.connect(host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
 cursor = conn.cursor()
